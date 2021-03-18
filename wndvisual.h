244a4661de3a1a3437d0dfe7dbdacb299e78718b
sean9892 <46587635+sean9892@users.noreply.github.com>
2021-03-18 13:15:31 +0900

    Update wndvisual.h

--- wndvisual.h
+++ wndvisual.h
@@ -4,5 +4,16 @@
 
 void txtCol(int foreground,int background){
 	int color=foreground+background*16;
-	//SetConsoleTextAttribute()
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);
+}
+
+void gotoxy(int x,int y){
+	COORD pos={x,y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); 
+}
+
+void wndSize(int xs,int ys){
+	char cmd[999];
+	sprintf(cmd,"mode con: cols=%d lines=%d",ys,xs);
+	system(cmd);
 }
