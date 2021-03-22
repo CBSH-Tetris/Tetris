763b69c1ca23a9c5d843aa3ecd326b94ce19db6c
sean9892 <46587635+sean9892@users.noreply.github.com>
2021-03-22 18:36:36 +0900

    update

--- room.h
+++ room.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#include "server.h"
+
 void joinRoom() {
     return;
 }
--- server.h
+++ server.h
@@ -10,14 +10,16 @@
 namespace server{
 	WORD        wVersionRequested;
 	WSADATA     wsaData;
-	SOCKADDR_IN servAddr, cliAddr;
+	SOCKADDR_IN servAddr, cliAddr, target;
 	SOCKET      s, s2;
 	int         err;
 	int         bytesSent;
 	char        buf[999];
+	int         isServ=-1;
 	
 	#define showError(s,e) std::cout<<"[X] "<<(s)<<" : "<<(e)<<"\n";
-	int makeConnection(const int PORT=12345){
+	int makeConnection_s(const int PORT=12345){
+		isServ=1;
 		wVersionRequested = MAKEWORD(1,1);
 		err = WSAStartup(wVersionRequested, &wsaData);
 		if(err!=0){
@@ -49,17 +51,44 @@ namespace server{
 		return 1;
 	}
 	
-	string recvTxt(){
-		int n = recv(s2,buf,999,0);
+	int makeConnection_c(const std::string IP,const int PORT=12345){
+		isServ=0;
+		wVersionRequested = MAKEWORD(1,1);
+		err = WSAStartup(wVersionRequested, &wsaData);
+		if(err!=0){
+			showError("WSAStartup Error",WSAGetLastError());
+			WSACleanup();
+			return 0;
+		}
+		target.sin_family = AF_INET;
+		target.sin_port = htons(PORT);
+		target.sin_addr.s_addr = inet_addr(IP.c_str());
+		
+		s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if(s==INVALID_SOCKET){
+			showError("Socket Error",WSAGetLastError());
+			WSACleanup();
+			return 0;
+		}
+		
+		if(connect(s,reinterpret_cast<SOCKADDR *>(&target),sizeof(target))==SOCKET_ERROR){
+			showError("connect() Error",WSAGetLastError());
+			WSACleanup();
+			return 0;
+		}
+	}
+	
+	std::string recvTxt(){
+		int n = recv(isServ?s2:s,buf,999,0);
 		if(n<=0){
 			return "";
 		}
 		buf[n]=0;
-		return string(buf);
+		return std::string(buf);
 	}
 	
-	int sendTxt(string str){
-		return send(s2,str.c_str(),str.size(),0);
+	int sendTxt(std::string str){
+		return send(isServ?s2:s,str.c_str(),str.size(),0);
 	}
 	#undef showError
 }
